9fab702b5dcf8b27b1a3718af3c81206a4ea8c44
Vlad Shevyakov <v.shevy@ya.ru>
2021-03-16 10:22:54 +0400

    Added security in backend

--- backend/src/main/java/ru/edjll/backend/controller/TestController.java
+++ backend/src/main/java/ru/edjll/backend/controller/TestController.java
@@ -1,16 +1,22 @@
 package ru.edjll.backend.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
 
 @RestController
 public class TestController {
 
-    @CrossOrigin("http://localhost:3000")
-    @GetMapping("/test")
-    public ResponseEntity<String> test() {
-        return ResponseEntity.ok().body("Hello, world!");
+    @GetMapping("/user")
+    public ResponseEntity<String> user(Principal principal) {
+        return ResponseEntity.ok().body("Hello, user, " + principal.getName() + "!");
+    }
+
+    @GetMapping("/admin")
+    public ResponseEntity<String> admin(Principal principal) {
+        return ResponseEntity.ok().body("Hello, admin, " + principal.getName() + "!");
     }
 }
